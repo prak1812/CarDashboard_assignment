6956235a322ad0e4cbc0daa000c29ef6be960c9f
Prakhar Sethi <prakharsethi05@gmail.com>
2026-06-11 22:26:49 +0530

    switched base

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://cardashboard-assignment-backend.onrender.com",
 });
 
 export default API;
